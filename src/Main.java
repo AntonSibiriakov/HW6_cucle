07c3c073aa4c11bd4daf65aa343ccc8ffb681b27
ThinkPad <Tonan041@gmail.com>
2022-11-21 18:36:42 +0100

    1234

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание №1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //Задание №2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        //Задание №3
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        //Задание №4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
